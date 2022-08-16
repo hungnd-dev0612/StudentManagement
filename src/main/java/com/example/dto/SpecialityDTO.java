08837adb9a776142be50accd61e1c9527d3f5922
DangHung <hungnd.dev0612@gmail.com>
2022-08-16 09:57:52 +0700

    make git dont push sonar change and put more annotation

--- src/main/java/com/example/dto/SpecialityDTO.java
+++ src/main/java/com/example/dto/SpecialityDTO.java
@@ -1,11 +1,15 @@
 package com.example.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class SpecialityDTO {
 
     private String name;
--- src/main/java/com/example/dto/StudentClassDTO.java
+++ src/main/java/com/example/dto/StudentClassDTO.java
@@ -2,12 +2,16 @@ package com.example.dto;
 
 
 import com.example.entities.SpecialityEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class StudentClassDTO {
     private String name;
     private SpecialityEntity major;
